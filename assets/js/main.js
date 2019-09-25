4ca2e8769cc9ad3d04ea23c32c3bf0cd45ced7bc
unknown <tim@timrich.org>
2019-09-25 17:55:17 +0300

    update

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var scrollTop =0;
+    $(window).scroll(function(){
+        scrollTop = $(window).scrollTop();
+        if (scrollTop > 200){
+            $('.navbar').addClass ('scrollNav');
+		
+			
+        }else if (scrollTop < 200){
+            $('.navbar').removeClass('scrollNav');
+			
+        };
+    });
+
+});
